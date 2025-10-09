57b60f90677f2b7c3e20413ef579398658272d81
KieenLee <Hello2112ha@gmail.com>
2025-10-09 23:44:43 +0700

    Update

--- fintech-tracker/fintechtracker-backend/obj/Debug/net9.0/fintechtracker-backend.AssemblyInfo.cs
+++ fintech-tracker/fintechtracker-backend/obj/Debug/net9.0/fintechtracker-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fintechtracker-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8edafe6eb8a76cbe5de0e1b2d96295b72f8ba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15fefc16b770ab6c1a955232e1e1759bb37fc66")]
 [assembly: System.Reflection.AssemblyProductAttribute("fintechtracker-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fintechtracker-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
